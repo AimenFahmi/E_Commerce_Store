c6c65dfb89030007167bb408d447f69a6c619e2e
FAHMI Aimen <aimen.fahmi@unifr.ch>
2020-03-29 08:12:09 +0200

    new client implementation

---
+++ clients/RegularClient.h
@@ -0,0 +1,10 @@
+//
+// Created by Aimen Fahmi on 2020-03-29.
+//
+
+#ifndef E_COMMERCE_STORE_REGULARCLIENT_H
+#define E_COMMERCE_STORE_REGULARCLIENT_H
+
+
+
+#endif //E_COMMERCE_STORE_REGULARCLIENT_H
